8a0ebc6ce47e1c545d6c4c199e23744dc9914f08
Bilal Khundmiri <bilalk@Bilals-MBP.home>
2021-01-28 12:55:46 -0500

    Fully Functional Project 1

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const getPlanets = async () => {
 // Display Data Call 2
 
 
-const earthFilterUrl = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=json&where=koi_prad>0.6 and koi_prad<1.5 and koi_teq>180 and koi_teq<303 and koi_disposition like 'CANDIDATE'&select=kepoi_name,kepler_name,koi_disposition,koi_pdisposition,koi_period,koi_prad,koi_teq,koi_insol,koi_steff"
+const earthFilterUrl = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=json&where=koi_prad>0.6 and koi_prad<1.5 and koi_teq>180 and koi_teq<303 and koi_pdisposition like 'CANDIDATE'&select=kepoi_name,kepler_name,koi_disposition,koi_pdisposition,koi_period,koi_prad,koi_teq,koi_insol,koi_steff"
 
 const getEarthFilterPlanets = async () => {
     removeData()
@@ -70,13 +70,13 @@ const removeData = () => {
 // Filter Listeners for Data
 
 
-const resetFilterData = document.querySelector('.filterReset')
+const resetFilterData = document.querySelector('.reset')
 resetFilterData.addEventListener('click', getPlanets)
 
-const earthFilterData = document.querySelector('.filterEarth')
+const earthFilterData = document.querySelector('.earth')
 earthFilterData.addEventListener('click', getEarthFilterPlanets)
 
-const habitableFilterData = document.querySelector('.filterHabitable')
+const habitableFilterData = document.querySelector('.habitable')
 habitableFilterData.addEventListener('click', getHabitableFilterPlanets)
 
 
